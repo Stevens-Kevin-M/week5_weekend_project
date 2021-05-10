8c5485360327c3ef6e8eeccd242536782a894e80
Kevin Stevens <stevens.kevin.m@gmail.com>
2021-05-10 14:18:41 -0500

    debugging project

---
+++ may_blog/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import Config
+from .site.routes import site
+from .authentication.routes import auth
+from flask_migrate import Migrate
+from may_blog.models import db as root_db, login_manager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+app.register_blueprint(site)
+app.register_blueprint(auth)
+
+root_db.init_app(app)
+migrate = Migrate(app, root_db)
+
+login_manager.init_app(app)
+
+from may_blog import models
